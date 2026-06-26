b9c9133cb4d7f1600cf4ea22f1e628f23a4a3e8c
tonogw <85993102+tonogw@users.noreply.github.com>
2026-06-26 12:00:09 +0700

    feat:created QNA data @constant

--- src/constant/faq-data.ts
+++ src/constant/faq-data.ts
@@ -1,32 +1,38 @@
 type FAQData = {
-  question: string;
-  answer: string;
+  id: number;
+  trigger: string;
+  content: string;
 };
 
 export const faqData: FAQData[] = [
   {
-    question: "What's your approach to fron-end development?",
-    answer:
+    id: 1,
+    trigger: "What's your approach to fron-end development?",
+    content:
       "I focus on clean, maintable code, and prioritize user experience. My approach involves close collaborations with designer to ensure exact implementation and seamless interactions all devices",
   },
   {
-    question: "How do you ensure website load quickly and eficiently?",
-    answer:
+    id: 2,
+    trigger: "How do you ensure website load quickly and eficiently?",
+    content:
       "I focus on clean, maintable code, and prioritize user experience. My approach involves close collaborations with designer to ensure exact implementation and seamless interactions all devices",
   },
   {
-    question: "What kind of projects do you specilize?",
-    answer:
+    id: 3,
+    trigger: "What kind of projects do you specilize?",
+    content:
       "I focus on clean, maintable code, and prioritize user experience. My approach involves close collaborations with designer to ensure exact implementation and seamless interactions all devices",
   },
   {
-    question: "How do you handle project deadline?",
-    answer:
+    id: 4,
+    trigger: "How do you handle project deadline?",
+    content:
       "I focus on clean, maintable code, and prioritize user experience. My approach involves close collaborations with designer to ensure exact implementation and seamless interactions all devices",
   },
   {
-    question: "What technologies do you use?",
-    answer:
+    id: 5,
+    trigger: "What technologies do you use?",
+    content:
       "Next.js, React js, Tailwind, Framer motion, TypeScipt, Monggo DB, Docker, Responsive",
   },
 ];
