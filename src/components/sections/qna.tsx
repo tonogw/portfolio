f3b6e76b1e4cc5d0f9568cf677e251dd3ba259f0
tonogw <85993102+tonogw@users.noreply.github.com>
2026-06-28 10:55:09 +0700

    feat:refactor darkMode

--- src/components/sections/qna.tsx
+++ src/components/sections/qna.tsx
@@ -13,7 +13,7 @@ export default function QnA() {
   return (
     <section
       id="faq"
-      className="relative max-w-360 mx-auto bg-white py-20 text-black overflow-hidden"
+      className="relative max-w-360 mx-auto bg-white dark:bg-neutral-900 py-20 text-black dark:text-white overflow-hidden"
     >
       {/* Header Judul */}
       <m.div
@@ -23,10 +23,10 @@ export default function QnA() {
         transition={{ duration: 0.6, ease: "easeOut" }}
         className="custom-container text-center flex flex-col items-center gap-3 mb-16"
       >
-        <span className="border border-[#D5D7DA] px-4 py-1.5 rounded-full text-xs font-bold text-gray-500 shadow-sm bg-white">
+        <span className="border border-[#D5D7DA] px-4 py-1.5 rounded-full text-xs font-bold text-gray-500 dark:text-white shadow-sm bg-white dark:bg-neutral-800">
           QNA
         </span>
-        <h2 className="text-4xl lg:text-[48px] font-black text-gray-900 tracking-tight mt-1">
+        <h2 className="text-4xl lg:text-[48px] font-black text-gray-900 dark:text-white tracking-tight mt-4">
           Your Questions, Answered
         </h2>
       </m.div>
@@ -36,7 +36,11 @@ export default function QnA() {
         {/* Induk Accordion diletakkan di luar looping agar manajemen open/close tersinkronisasi */}
         <Accordion type="single" collapsible className="w-full">
           {faqData.map((item) => (
-            <AccordionItem key={item.value} value={item.value}>
+            <AccordionItem
+              key={item.value}
+              value={item.value}
+              className="dark:bg-gray-200"
+            >
               {/* Baris 1: Pertanyaan (Hanya berisi teks pemicu klik) */}
               <AccordionTrigger>{item.trigger}</AccordionTrigger>
 
